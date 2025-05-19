8c586c2bdbfbb7a7bd4d0be8d8c99fb451acd76f
aleksanderhady <60362660+aleksanderhady@users.noreply.github.com>
2025-05-19 14:58:08 +0200

    Update problem-solving.py

--- problem-solving.py
+++ problem-solving.py
@@ -14,7 +14,7 @@ class Person:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
-        self.age == age
+        self.age = age # Typo; we are assigning, not comparing, hence '=", not '=='
         self.id = self.increase_count()
 
     # This method should not be modified.
@@ -22,21 +22,25 @@ class Person:
         time.sleep(1)
         print(f"Hello, my first name is {self.first_name} and I am {self.age} years old.")
 
-    def increase_count():
-        Person.people_count += 1
-        return Person.people_count
+    @classmethod # Added classmethod to ensure people_count is incremented consistently across all class instances
+    def increase_count(cls):
+        cls.people_count += 1
+        return cls.people_count
 
 def main():
     threads = []
     for p in people:
-        x = Person(p["first_name"], p["age"], p["last_name"])
+        x = Person(p["first_name"], p["last_name"], p["age"])  # Fixed order according to the constructor
         threads.append(threading.Thread(target=x.introduce))
         
     for thread in threads:
         thread.start()
+
+    for thread in threads: # Number of people needs to be printed at the very end so we wait for all threads
+        thread.join()
     
     print(f"Number of people created: {Person.people_count}")
     return
 
-if __name__ == "main":
+if __name__ == "__main__": # Fixed syntax
     main()
